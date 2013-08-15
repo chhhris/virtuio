516d44159901e05e90cccf379196bc14a05d59f0
chhhris <mr.lake@gmail.com>
2013-08-15 07:53:25 -0400

    Try Stripe key with single quotes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => sk_test_avtQwlBh8TDFQJ7ocTwZpfFX,
-  :secret_key      => pk_test_9ei9lPR5eL6F2B6sXnMuPZF6
+  :publishable_key => 'sk_test_avtQwlBh8TDFQJ7ocTwZpfFX',
+  :secret_key      => 'pk_test_9ei9lPR5eL6F2B6sXnMuPZF6'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
